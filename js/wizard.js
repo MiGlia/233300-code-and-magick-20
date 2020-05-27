07aae6d0d05231568fd80b322dbf1412ac84d2ad
MiGlia <mishaglansev>
2020-05-27 16:10:22 +0300

    Изменяем поведение мага

--- js/wizard.js
+++ js/wizard.js
@@ -1,5 +1,24 @@
 'use strict';
 (function () {
   var fireballSize = 22;
-  
-})()
+  var wizardSpeed = 3;
+  var wizardWidth = 70;
+  var COIF_WIDTH = 1.377;
+
+
+  function getWizardHeight(width) {
+    return wizardHeight = COIF_WIDTH * width;
+  }
+
+  function getFireballSpeed(IsWindFromLeft) {
+    return IsWindFromLeft ? 2 : 5;
+  }
+
+  function getWizardX (gameFieldWidth) {
+    return gameFieldWidth/2;
+  }
+
+  function getWizardY (gameFieldHeight) {
+    return gameFieldHeight/3 - wizardHeight;
+  }
+})();
