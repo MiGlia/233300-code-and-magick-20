cfd2a2e24dff86cb63386c4001c7f73abe315cae
MiGlia <mishaglansev>
2020-06-09 17:21:51 +0300

    функции для клонирования шаблона, заполнение его данными и отрисовке на странице

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,10 @@
   var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
   var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', ' Топольницкая', 'Нионго', ' Ирвинг'];
+  var userDialog = document.querySelector('.setup');
+
+  userDialog.classList.remove('hidden');
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
   function getRandomValue(min, max) {
     return Math.random() * (max - min) + min;
@@ -40,6 +44,27 @@
     return arr;
   }
 
-  createNewArrObject(5, wiz, WIZARD_NAMES, WIZARD_EYES_COLORS, WIZARD_COAT_COLORS);
+  createNewArrObject(4, wiz, WIZARD_NAMES, WIZARD_EYES_COLORS, WIZARD_COAT_COLORS);
 
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+  var fragment = document.createDocumentFragment();
+
+  function renderWizard(arr) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').name = arr.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = arr.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = arr.eyesColor;
+    return wizardElement;
+  }
+
+  function drawWizards(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      fragment.appendChild(renderWizard(arr[i]));
+      similarListElement.appendChild(fragment);
+    }
+  }
+  drawWizards(wiz);
 })();
